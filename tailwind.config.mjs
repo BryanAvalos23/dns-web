25c33dfc78757d259f62bbcf3b9834e72693595a
Bryan Avalos <bryanavalos23@outlook.com>
2024-08-08 01:32:07 -0600

    feat: Add new feat complete, page index completed
    Se finalizo el desarrollo de la apgina de index del proyecto

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        'server': "url('/images/server.jpg')",
+        'server': "url('/images/server.webp')",
       }
     },
   },
